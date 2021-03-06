e2a620be3bf4341f31d01617ad0c5df3b3a43c8d
Olga <ms.estete@gmail.com>
2021-03-06 16:57:31 +0200

    updated user case

--- src/user/User.js
+++ src/user/User.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { fetchStates } from "FiniteStateMachine";
+import Machine from "FiniteStateMachine";
+import Loader from "react-loader-spinner";
 
-class User extends React.Component {
-  state = {};
-  async componentDidMount() {
-    const fetchedData = fetchStates();
-    this.setState({ fetchedData });
-  }
+const User = () => {
+  const { loading, error, results } = Machine("http://localhost:3000/users");
+  return loading ? (
+    <Loader type="Circles" color="#00BFFF" height={80} width={80}></Loader>
+  ) : error ? (
+    <h3 className="error"> {"User not found"} </h3>
+  ) : (
+    results.map((result, index) => <UserDetails data={result} key={index}/>)
+  );
+};
 
-  render() {
-    return this.state.loading ? (
-      <div>"I am loading"</div>
-    ) : this.state.error ? (
-      <div>"I am error"</div>
-    ) : (
-      <div> {this.state.results}</div>
-    );
-  }
-}
+const UserDetails = ({ data: { first_name, last_name, email, gender }}) => {
+  return (
+    <div className="details-container">
+      <h4>{first_name}</h4>
+      <h4>{last_name}</h4>
+      <h4>{email}</h4>
+      <h4>{gender}</h4>
+    </div>
+  );
+};
 
 export default User;
